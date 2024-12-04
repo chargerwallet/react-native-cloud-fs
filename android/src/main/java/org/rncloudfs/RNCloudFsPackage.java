32a972f2d240384e452cca1f8cadf4e4af2eb9e5
lyfeloopinc <support@lyfeloop.com>
2024-12-03 19:05:51 -0500

    commit main

---
+++ android/src/main/java/org/rncloudfs/RNCloudFsPackage.java
@@ -0,0 +1,28 @@
+package org.rncloudfs;
+
+import java.util.*;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+public class RNCloudFsPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      List<NativeModule> modules = new ArrayList<>();
+      modules.add(new RNCloudFsModule(reactContext));
+      return modules;
+    }
+
+    // deprecated >= RN 0.47.0
+    public List<Class<? extends JavaScriptModule>> createJSModules() {		
+        return Collections.emptyList();		
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+      return Arrays.<ViewManager>asList();
+    }
+}
